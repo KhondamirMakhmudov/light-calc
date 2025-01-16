4ae81c91a93722d6206e162593fb5d3e50e9798e
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2025-01-16 16:39:36 +0500

    fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL: "https://cal.mkinfo.uz/api/v1/",
+  API_URL: "http://cal.mkinfo.uz:8033/api/v1/",
   // FILE_URL: "https://backend-market.tmsiti.uz/static/categories/materials/",
   DEFAULT_APP_LANG: "uz",
 };
